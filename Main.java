7c5eb7bcf5fc6ad5659e6ca84ab0954f0073e88d
Nadarajan GS <nadarajangs31@gmail.com>
2023-03-31 08:40:15 +0000

    simple calculator

--- Main.java
+++ Main.java
@@ -1,45 +1,54 @@
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
-        System.out.print("Enter some basic operator:");
-        Scanner in = new Scanner(System.in);
-        char op = in.next().trim().charAt(0);  
-        System.out.println("Enter the two input numbers:");
-        
-        if(op == '+' || op == '-' || op == '*' || op == '/' ) {
-            int num1 = in.nextInt();
-            int num2 = in.nextInt();
-            if(op == '+'){
+       
+        while(true){
+            System.out.print("Enter some basic operator:");
+            Scanner in = new Scanner(System.in);
+            char op = in.next().trim().charAt(0);  
+            System.out.print("Enter the two input numbers:");
+            if(op == '+' || op == '-' || op == '*' || op == '/' ) {
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+                if(op == '+'){
+                   
+                    int add = num1+num2;
+                    System.out.println("The sum value is "+ add+"\n");
+                }
+                if(op == '-'){
+                    System.out.println("Enter the two input numbers:");
+                    int sub = num1-num2;
+                    System.out.print("The sub value is "+ sub +"\n");
+                }
+                if(op == '*'){
+                    System.out.println("Enter the two input numbers:");
+                    int product = num1*num2;
+                    System.out.print("The product value is "+ product +"\n");
+                }
+                if(op == '/'){ 
+                   if(num2 != 0 ){
+                    System.out.println("Enter the two input numbers:");
+                    int quotient = num1 /num2;
+                    System.out.print("The quotient value is "+ quotient +"\n");
+                   }
+                    
+                }
                
-                int add = num1+num2;
-                System.out.println("The sum value is "+ add+"\n");
+    
             }
-            if(op == '-'){
-                System.out.println("Enter the two input numbers:");
-                int sub = num1-num2;
-                System.out.print("The sub value is "+ sub +"\n");
+            else if (op == 'x'){
+                break;
             }
-            if(op == '*'){
-                System.out.println("Enter the two input numbers:");
-                int product = num1*num2;
-                System.out.print("The product value is "+ product +"\n");
+               
+            else{
+                System.out.print("Please enter some valid operator !");
+                break;
+              
             }
-            if(op == '/'){ 
-               if(num2 != 0 ){
-                System.out.println("Enter the two input numbers:");
-                int quotient = num1 /num2;
-                System.out.print("The quotient value is "+ quotient +"\n");
-               }
-                
-            }
-           
 
         }
-           
-        else{
-            System.out.print("Please enter some valid operator !");
-          
-        }
+        
+  
 
         }
             
